6e1f2a244081197ba60e53bc89a5842937b4a32e
chiseledCoder <nikhil.salome@gmail.com>
2020-04-21 07:43:54 +0530

    search_for_summaries done

--- the_app/tests.py
+++ the_app/tests.py
@@ -1,3 +1,19 @@
+import json
 from django.test import TestCase
+from .views import search_for_summaries
 
 # Create your tests here.
+
+
+with open('utils/data.json') as book_data:
+    data = book_data.read()
+summaries = json.loads(data)['summaries']
+
+
+class SearchEngineTesting(TestCase):
+
+    def empty_query_passed(self):
+        """Function should return entire list of summaries if query string is empty."""
+        query = "gg"
+        k = 2
+        self.assertEqual(search_for_summaries(query=query, k=k), summaries)
--- the_app/views.py
+++ the_app/views.py
@@ -10,7 +10,7 @@ query = "is your problems"
 k = 3
 
 
-def search_for_summaries(request, query=query, k=k):
+def search_for_summaries(query, k):
     """ Reads json and return best k number of the most relevant summaries of the books.
     The query is converted to list of keywords. Every summary is iterated and it is split in to keywords.
     Another iterator is run for len(summary) - len(query) to handle IndexError.
@@ -21,4 +21,25 @@ def search_for_summaries(request, query=query, k=k):
     with open('utils/data.json') as book_data:
         data = book_data.read()
     summaries = json.loads(data)['summaries']
-    return JsonResponse({"summaries": summaries})
+    if len(query) == 0:  # if query is empty then return the summaries without searching.
+            return summaries
+    query = query.lower()  # we lower the string to to make query search case-insensitive.
+    query_length = len(query.split(" "))  # we get list of kw from the query.
+    temp = dict()  # dict to store the relevant summary and it's related information.
+
+    for item in summaries:  # iterating over the summaries.
+        count = 0  # count get initialized for every summary
+        words = item['summary'].lower().split(" ")  # we convert the summary into list of kw
+        summary_length = len(words)  # length of each summary
+        for i in range(summary_length - query_length):  # we iterate from 0 to
+            if query in " ".join(words[i: i + query_length]):  # partial matching in the window size of len(query)
+                count += 1  # if a match is found we increment the count
+
+            # we append the
+            temp[item['id']] = {"data": {"summary": item['summary'], "id": item['id']},
+                                "rank": (count / summary_length) * 100}
+
+    data = [i['data'] for i in dict(sorted(temp.items(),
+                                           key=lambda val: val[1]['rank'],
+                                           reverse=True)[:k]).values()]
+    return json.dumps(data)
